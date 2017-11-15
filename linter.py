b443366a0cc55c869e6639d3a16bee86fac9352f
martin <ivan.filenko@protonmail.com>
2017-11-15 23:53:31 +0300

    Format json output to text

--- linter.py
+++ linter.py
@@ -10,6 +10,7 @@
 
 """This module exports the Dockerfilelint plugin class."""
 
+import json
 from SublimeLinter.lint import Linter, util
 
 
@@ -34,4 +35,20 @@ class Dockerfilelint(Linter):
     inline_settings = None
     inline_overrides = None
     comment_re = None
+
+    def run(self, cmd, code):
+        output = super().run(self, cmd, code)
+        return self.format(output)
+
+    def format(self, output):
+        """Formats json output to text <file>:<line>:<error>:<message>\n"""
+
+        json = json.loads(output)
+        filename = json['files'][0]['file']
+        formatted_lines = []
+
+        for issue in json['files'][0]['issues']:
+            message = issue['description'].split('.')[0]
+            formatted_lines.append(''.join([filename, ':', issue['line'], ':', issue['category'], ':', message, "\n"]))
+        return ''.join(formatted_lines)
 
